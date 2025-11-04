bc4e0245d098adcd2c174456e5da932dd38bda93
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-11-04 00:48:00 -0300

    correção no template de Controller

--- templates/Controller.php
+++ templates/Controller.php
@@ -3,6 +3,8 @@
 namespace CustomEntity\Controllers;
 
 use MapasCulturais\Controllers\EntityController;
+use MapasCulturais\Traits as CoreTraits;
+use CustomEntity\Traits as Traits;
 
 class _ENTITY_NAME_ extends EntityController
 {
