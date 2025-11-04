57276ed2093b648d6eeae7e0cb07bc4f4320ceb9
israelmelo <israelg.8@gmail.com>
2025-11-04 15:04:44 -0300

    Ajusta part ShortDescription

--- Parts/ShortDescription.php
+++ Parts/ShortDescription.php
@@ -2,8 +2,10 @@
 
 namespace CustomEntity\Parts;
 
+use CustomEntity\EntityDefinition;
 use CustomEntity\Part;
 use CustomEntity\Traits;
+use MapasCulturais\App;
 use MapasCulturais\i;
 
 class ShortDescription extends Part
@@ -18,9 +20,18 @@ class ShortDescription extends Part
     public function getEntityValidations(): array
     {
         return [
-            'name' => [
+            'shortDescription' => [
                 'required' => i::__('A descrição curta é obrigatória')
             ],
         ];
+    }
+
+    public function init(EntityDefinition $entity_definition)
+    {
+        $app = App::i();
+        $app->hook("template({$entity_definition->slug}.edit.tab-info--content):begin", function (){
+            /** @var Theme $this */
+            $this->part('custom-entity/edit/short-description');
+        });
     }
 }
---
+++ layouts/parts/custom-entity/edit/short-description.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+use MapasCulturais\i;
+
+$this->import('entity-field');
+?>
+<div class="col-12 grid-12">
+    <entity-field :entity="entity" classes="col-12" prop="shortDescription" :max-length="400"></entity-field>
+</div>
+
