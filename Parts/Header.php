376cc90b125ec147d1b18c59c464838aeb74005d
israelmelo <israelg.8@gmail.com>
2025-11-05 11:27:50 -0300

    Cria part Header

---
+++ Parts/Header.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CustomEntity\Parts;
+
+use CustomEntity\EntityDefinition;
+use CustomEntity\Part;
+use MapasCulturais\App;
+use MapasCulturais\Definitions\FileGroup;
+use MapasCulturais\i;
+use MapasCulturais\Themes\BaseV2\Theme;
+
+class Header extends Part
+{
+    public function getSubParts(): array
+    {
+        return [
+            Files::create()
+        ];
+    }
+
+    public function getFileGroups(): array
+    {
+        return [
+            new FileGroup('header', ['^image/(jpeg|png)$'], i::__('O arquivo enviado não é uma imagem válida.'), true),
+        ];
+    }
+
+    public function init(EntityDefinition $entity_definition)
+    {
+        $app = App::i();
+        
+        $app->hook("template({$entity_definition->slug}.edit.tab-info--content):begin", function () use ($entity_definition) {
+            /** @var Theme $this */
+            $this->part('custom-entity/edit/header');
+        });
+    }
+}
+
---
+++ layouts/parts/custom-entity/edit/header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+$this->import('entity-cover');
+?>
+
+<entity-cover :entity="entity" classes="col-12"></entity-cover>
