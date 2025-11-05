fa39553536563ea7833dac3f5910641c338cb802
israelmelo <israelg.8@gmail.com>
2025-11-05 16:11:32 -0300

    Cria part Administrators

---
+++ Parts/Administrators.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CustomEntity\Parts;
+
+use CustomEntity\EntityDefinition;
+use CustomEntity\Part;
+use MapasCulturais\App;
+use MapasCulturais\i;
+use MapasCulturais\Themes\BaseV2\Theme;
+
+class Administrators extends Part
+{
+    public function getSubParts(): array
+    {
+        return [
+            AgentRelations::add()
+        ];
+    }
+
+    public function init(EntityDefinition $entity_definition)
+    {
+        $app = App::i();
+        
+        $app->hook("template({$entity_definition->slug}.edit.tab-info--content):end", function () {
+            /** @var Theme $this */
+            $this->part('custom-entity/edit/administrators');
+        });
+        
+        $app->hook("template({$entity_definition->slug}.single.tab-info--container):begin", function () {
+            /** @var Theme $this */
+            $this->part('custom-entity/single/administrators');
+        });
+    }
+}
+
