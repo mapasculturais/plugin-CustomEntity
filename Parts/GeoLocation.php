8c0857185fd61427409d32354cb1609449450ee4
israelmelo <israelg.8@gmail.com>
2025-11-12 16:47:55 -0300

    Ajustes na part GeoLocation

--- Parts/GeoLocation.php
+++ Parts/GeoLocation.php
@@ -7,9 +7,19 @@ use CustomEntity\Part;
 use MapasCulturais\App;
 use MapasCulturais\i;
 use MapasCulturais\Traits as MapasTraits;
+use MapasCulturais\Types\GeoPoint;
 
 class GeoLocation extends Part
 {
+    protected bool $showLatLongFields = false;
+
+    public function showLatLongFields(bool $show = true): static
+    {
+        $this->showLatLongFields = $show;
+
+        return $this;
+    }
+
     public function getEntityTraits(): array
     {
         return [
@@ -19,19 +29,66 @@ class GeoLocation extends Part
 
     public function getEntityValidations(): array
     {
+        if (!$this->isRequired) {
+            return [];
+        }
+
+        $callback_validation = 'v::callback("' . self::class . '::validateLocation")';
+
         return [
             'location' => [
-                'required' => i::__('A localização é obrigatória')
+                'required' => i::__('A localização é obrigatória'),
+                $callback_validation => i::__('Latitude e longitude não podem ser 0,0')
             ],
         ];
+    }
+
+    public static function validateLocation($location): bool
+    {
+        if (!$location) {
+            return false;
+        }
+
+        if ($location instanceof GeoPoint) {
+            $lat = $location->latitude;
+            $lng = $location->longitude;
+        } elseif (is_array($location)) {
+            $lat = $location['lat'] ?? $location['latitude'] ?? ($location[1] ?? null);
+            $lng = $location['lng'] ?? $location['longitude'] ?? ($location[0] ?? null);
+        } elseif (is_object($location)) {
+            $lat = $location->lat ?? $location->latitude ?? null;
+            $lng = $location->lng ?? $location->longitude ?? null;
+        } else {
+            return false;
+        }
+
+        if ($lat == null || $lng == null) {
+            return false;
+        }
+
+        return (float) $lat != 0.0 || (float) $lng != 0.0;
     }
 
     public function init(EntityDefinition $entity_definition)
     {
         $app = App::i();
-        $app->hook("template({$entity_definition->slug}.edit.tab-info--content):end", function () {
+        $self = $this;
+
+        $app->hook("template({$entity_definition->slug}.edit.tab-info--content):end", function () use ($self) {
             /** @var Theme $this */
-            $this->part('custom-entity/edit/geo-location');
+            $this->part('custom-entity/edit/geo-location', [
+                'showLatLongFields' => $self->showLatLongFields,
+            ]);
+        });
+
+        $app->hook("template(<<*>>.<<*>>.create-{$entity_definition->slug}__fields):begin", function () use ($self) {
+            /** @var Theme $this */
+
+            if ($self->isRequired) {
+                $this->part('custom-entity/edit/geo-location', [
+                    'showLatLongFields' => $self->showLatLongFields,
+                ]);
+            }
         });
 
         $app->hook("template(search.{$entity_definition->slug}.search-custom-entity):end", function () use ($app) {
