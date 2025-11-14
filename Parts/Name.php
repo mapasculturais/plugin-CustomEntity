0ff374944da37e0c8cdf5409ac0fba2697c06e8d
israelmelo <israelg.8@gmail.com>
2025-11-14 09:02:53 -0300

    Ajusta part name para possibilitar ser um campo select

--- Parts/Name.php
+++ Parts/Name.php
@@ -6,10 +6,61 @@ use CustomEntity\EntityDefinition;
 use CustomEntity\Part;
 use CustomEntity\Traits;
 use MapasCulturais\App;
+use MapasCulturais\Entity as MapasEntity;
 use MapasCulturais\i;
+use MapasCulturais\Themes\BaseV2\Theme;
 
 class Name extends Part
 {
+    protected ?string $fieldType = null;
+    protected array $options = [];
+    protected array $optionsOrder = [];
+
+    public function __construct(?array $config = null)
+    {
+        if (is_array($config)) {
+            if (isset($config['fieldType'])) {
+                $this->fieldType($config['fieldType']);
+            }
+
+            if (isset($config['options'])) {
+                $this->options($config['options']);
+            }
+        }
+    }
+
+    public function fieldType(string $field_type): static
+    {
+        $this->fieldType = trim($field_type);
+
+        return $this;
+    }
+
+    public function options(array $options): static
+    {
+        $formatted = [];
+        $order = [];
+
+        foreach ($options as $value => $label) {
+            if (is_array($label) || is_object($label)) {
+                continue;
+            }
+
+            $formatted_value = is_int($value) ? (string) $value : (string) $value;
+            $formatted[$formatted_value] = (string) $label;
+            $order[] = $formatted_value;
+        }
+
+        $this->options = $formatted;
+        $this->optionsOrder = $order;
+
+        if (!$this->fieldType) {
+            $this->fieldType = 'select';
+        }
+
+        return $this;
+    }
+
     public function getEntityTraits(): array
     {
         return [
@@ -29,6 +80,8 @@ class Name extends Part
     public function init(EntityDefinition $entity_definition)
     {
         $app = App::i();
+        $self = $this;
+        
         $app->hook("template({$entity_definition->slug}.edit.tab-info--content):begin", function () {
             /** @var Theme $this */
             $this->part('custom-entity/edit/name');
@@ -37,6 +90,18 @@ class Name extends Part
         $app->hook("template(<<*>>.<<*>>.create-{$entity_definition->slug}__fields):begin", function () {
             /** @var Theme $this */
             $this->part('custom-entity/edit/name');
+        });
+
+        $app->hook("entity(CustomEntity.Entities.{$entity_definition->entity}).propertiesMetadata", function (&$properties) use ($self) {
+            if($self->fieldType && $self->fieldType == 'select') {
+                if (!isset($properties['name'])) {
+                    return;
+                }
+
+                $properties['name']['field_type'] = $self->fieldType;
+                $properties['name']['options'] = $self->options;
+                $properties['name']['optionsOrder'] = $self->optionsOrder;
+            }
         });
     }
 }
